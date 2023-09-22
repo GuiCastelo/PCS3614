4682755985c3b680e3f65efe035a32afc1d99273
GuiCastelo <guilherme.castelo@usp.br>
2023-09-22 09:35:56 -0300

    refactor(client-server): implement http connection between server-client

--- src/client.py
+++ src/client.py
@@ -1,22 +1,25 @@
 import socket
-import sys
 
-PORT = 80
+MAX_LINE = 256
 
 def main():
-    # Host deve ser passado via linha de comando
-    if(len(sys.argv) == 2):
-        hostname = sys.argv[1]
-    else:
-        raise AttributeError('usage: simple-talk host')
+    # Host deve ser passado via input
+    address = input("Digite o host a ser buscado, no formato hostname:port/file: ")
+
+    hostname = address.split(':')[0]
+    port = address.split(':')[-1].split('/')[0]
+    file = address.split('/', 1)[-1]
+
+    print(hostname, port, file)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         host = socket.gethostbyname(hostname)
-        s.connect((host, PORT))
-        data = input()
-        while data:
-            s.send(bytes(data, 'UTF-8'))
-            data = input()
+        s.connect((host, int(port)))
+        data = f'GET /{file} HTTP/1.1\r\nHost:{host}\r\n\r\n'
+        s.send(bytes(data, 'UTF-8'))
+        response = s.recv(MAX_LINE)
+        print(response)
+        s.close()
 
 if __name__ == '__main__':
     main()
--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 PORT = 80
 MAX_LINE = 256
@@ -10,11 +11,29 @@ def main():
         s.listen(MAX_PENDING)
         conn, addr = s.accept()
         with conn:
-            while True:
-                data = conn.recv(MAX_LINE)
-                print(str(data, 'UTF-8'))
-                if not data:
-                    break
+            request = conn.recv(MAX_LINE)
+            request = str(request, 'UTF-8')
+            filename = f'.{request.split(" ")[1]}'
+            try:
+                results = find_file(filename)
+                response = f'HTTP/1.1 200 OK\r\nContent-Length:{results["size"]}\r\nContent-Type:text/html\r\n\r\n{results["bytes"]}'
+                response = bytes(response, 'UTF-8')
+            except:
+                response = b'HTTP/1.1 400 ERRO\r\n\r\n'
+            finally:
+                conn.sendall(response)
+        s.close()
+
+def find_file(filename):
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as file:
+            bytes_file = file.read()
+        return {
+            'size': os.path.getsize(filename),
+            'bytes': bytes_file
+        }
+    else:
+        raise FileNotFoundError(f'{filename} not found in the server')
 
 if __name__ == '__main__':
     main()
