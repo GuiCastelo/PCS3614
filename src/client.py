17e87cecacbd8ee10dd6a32efce840b380724153
GuiCastelo <guilherme.castelo@usp.br>
2023-11-09 11:29:20 -0300

    feat(ipv6): add ipv6 support for client and server

--- src/client.py
+++ src/client.py
@@ -6,18 +6,28 @@ def main():
     # Host deve ser passado via input
     address = input("Digite o host a ser buscado, no formato hostname:port/file: ")
 
-    hostname = address.split(':')[0]
+    hostname = ':'.join(address.split(':')[0:-1])
     port = address.split(':')[-1].split('/')[0]
     file = address.split('/', 1)[-1]
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        host = socket.gethostbyname(hostname)
-        s.connect((host, int(port)))
-        request = f'GET /{file} HTTP/1.1\r\nHost:{host}\r\n\r\n'
-        s.send(bytes(request, 'UTF-8'))
-        response = s.recv(MAX_LINE)
-        print(response)
-        s.close()
+    try:
+        family, _, _, _, host = socket.getaddrinfo(hostname, port, socket.AF_UNSPEC, socket.SOCK_STREAM)[0]
+    except:
+        print('Host não encontrado')
+        exit()
+    
+    with socket.socket(family, socket.SOCK_STREAM) as s:
+        try:
+            s.connect(host)    
+            print(f'Conexão feita usando {"IPV6" if family == socket.AF_INET6 else "IPV4"}')
+            request = f'GET /{file} HTTP/1.1\r\nHost:{host}\r\n\r\n'
+            s.send(bytes(request, 'UTF-8'))
+            response = s.recv(MAX_LINE)
+            print(f'Resposta: {response.decode()}')
+            s.close()
+        except ConnectionRefusedError:
+            print('Conexão recusada pelo servidor')
+            exit()
 
 if __name__ == '__main__':
     main()
--- src/server.py
+++ src/server.py
@@ -6,24 +6,28 @@ MAX_LINE = 512
 MAX_PENDING = 5
 
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    family = socket.AF_INET6 if input('Inicializar servidor para conexões IPV6?(y/n): ') == 'y' else socket.AF_INET 
+    with socket.socket(family, socket.SOCK_STREAM) as s:
         s.bind(('', PORT))
         s.listen(MAX_PENDING)
-        conn, addr = s.accept()
-        with conn:
-            request = conn.recv(MAX_LINE)
-            request = str(request, 'UTF-8')
-            filename = f'.{request.split(" ")[1]}'
-            try:
-                results = find_file(filename)
-                response = f'HTTP/1.1 200 OK\r\nContent-Length:{results["size"]}\r\nContent-Type:text/html\r\n\r\n{results["bytes"]}'
-                response = bytes(response, 'UTF-8')
-            except:
-                response = b'HTTP/1.1 404 ERRO\r\n\r\n'
-            finally:
-                conn.sendall(response)
-                conn.close()
-        s.close()
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print(f'Conexão estabelecida para cliente {addr[0]}')
+                request = conn.recv(MAX_LINE)
+                request = str(request, 'UTF-8')
+                filename = f'.{request.split(" ")[1]}'
+                try:
+                    results = find_file(filename)
+                    response = f'HTTP/1.1 200 OK\r\nContent-Length:{results["size"]}\r\nContent-Type:text/html\r\n\r\n{results["bytes"].decode()}'
+                    response = bytes(response, 'UTF-8')
+                except FileNotFoundError:
+                    response = b'HTTP/1.1 404 ERRO\r\n\r\n'
+                except:
+                    response = b'HTTP/1.1 500 ERRO\r\n\r\n'
+                finally:
+                    conn.sendall(response)
+                    conn.close()
 
 def find_file(filename):
     if os.path.isfile(filename):
